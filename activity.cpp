2e7a7add08d067a6c3fa4f4da8c27c71e22d9a25
Alex Heidorn <heido023@d.umn.edu>
2024-11-07 15:47:22 -0600

    create activities class files. update cmake verison

---
+++ activity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by saga on 9/23/2024.
+//
+
+#include "activity.h"
---
+++ activity.h
@@ -0,0 +1,16 @@
+//
+// Created by saga on 9/23/2024.
+//
+
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+
+
+
+class activity {
+
+};
+
+
+
+#endif //ACTIVITY_H
