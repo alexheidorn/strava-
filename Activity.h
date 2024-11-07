8a5ff123012c2ee3a3a0ce169d3bd99cff22b6ab
Alex Heidorn <heido023@d.umn.edu>
2024-11-07 16:18:36 -0600

    add more fields

--- Activity.h
+++ Activity.h
@@ -9,12 +9,29 @@
 
 
 class Activity {
-private:
-    int id;
-    std::string name;
-    double distance;
-    std::chrono::system_clock::time_point startTime;
-    std::chrono::system_clock::time_point endTime;
+    private:
+        int id;
+        std::string name;
+        double distance;
+        int movingTime;
+        int elapsedTime;
+        std::string type; // ride, run, etc.
+        std::string sportType; // Mountain Bike, Trail Run, etc.
+        std::string workoutType; // race, commute, workout, etc.
+        std::chrono::system_clock::time_point startTime;
+        std::chrono::system_clock::time_point endTime;
+
+        // /* OPTIONAL PARAMS FOR LATER */
+        // / startDateLocal;
+        // / avgCadence
+        // / avgWatts
+        // / avgHR
+        // / maxHR
+        // / maxSpeed
+
+    public:
+
+
 
 
 };
