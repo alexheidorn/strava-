9116e6090259b140f8f53b27ac74113d57ff1230
Alex Heidorn <heido023@d.umn.edu>
2024-11-08 17:04:30 -0600

    replace old chatGPT variables with new ones

--- CurlRequests.cpp
+++ CurlRequests.cpp
@@ -1,9 +1,10 @@
 //
-// Created by alexh on 11/8/2024.
+// Created by saga on 11/8/2024.
 //
 
 #include "CurlRequests.h"
 
+#include <iostream>
 
 
 void CurlRequests::requestAccessToken(const std::string access_token) {
@@ -20,14 +21,14 @@ void CurlRequests::makeRequest() {
 
     // Set up the callback function to capture the response
     curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, &readBuffer);
 
     // Perform the request
-    res = curl_easy_perform(curl);
+    curlResult = curl_easy_perform(curlHandle);
 
     // Check for errors
-    if(res != CURLE_OK) {
-        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+    if(curlResult != CURLE_OK) {
+        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(curlResult) << std::endl;
     } else {
         std::cout << "Logged-in Athlete Information:\n" << readBuffer << std::endl;
     }
