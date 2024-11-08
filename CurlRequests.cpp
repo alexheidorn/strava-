addc0f10114b315ae63075197eef7ba80bcb4814
Alex Heidorn <heido023@d.umn.edu>
2024-11-08 16:53:02 -0600

    move callback into .h. create ctor to initialize curl & dtor to handle clean up. remove params from fnx because they're all member methods. continue copying code from main into methods in .cpp

--- CurlRequests.cpp
+++ CurlRequests.cpp
@@ -4,8 +4,31 @@
 
 #include "CurlRequests.h"
 
-// Callback function to handle the API response
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+
+
+void CurlRequests::requestAccessToken(const std::string access_token) {
+}
+
+void CurlRequests::setHeaders() {
+    curlHeaders = curl_slist_append(curlHeaders, ("Authorization: Bearer " + access_token).c_str());
+    curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, curlHeaders);
+}
+
+void CurlRequests::makeRequest() {
+    // Set the URL for the Strava API endpoint to retrieve the logged-in athlete's details
+    curl_easy_setopt(curlHandle, CURLOPT_URL, stravaAthlete);
+
+    // Set up the callback function to capture the response
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+    // Perform the request
+    res = curl_easy_perform(curl);
+
+    // Check for errors
+    if(res != CURLE_OK) {
+        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+    } else {
+        std::cout << "Logged-in Athlete Information:\n" << readBuffer << std::endl;
+    }
 }
--- CurlRequests.h
+++ CurlRequests.h
@@ -8,27 +8,38 @@
 #include <curl/curl.h>
 #include <string>
 
+// Callback function to handle the API response
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 class CurlRequests {
 private:
     // Replace this with your Strava API access token
     std::string access_token = "93fa4b6c3a39d4ee147dc7ca952fd51412162394";
     std::string readBuffer;
-    CURL* curlHandle = curl_easy_init();
+    CURL* curlHandle;
     CURLcode curlResult;
 
     struct curl_slist* curlHeaders = NULL;
 
 
-    // strava URL requests
-    std::string stravaGetAthlete = "https://www.strava.com/api/v3/athlete";
-
-
 public:
-    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+    CurlRequests() {
+        curlHandle = curl_easy_init();
+    }
+    ~CurlRequests() {
+        curl_easy_cleanup(curlHandle);
+        curl_slist_free_all(curlHeaders);
+    }
+    // strava URL requests
+    std::string stravaAthlete = "https://www.strava.com/api/v3/athlete";
+    std::string stravaActivity = "https://www.strava.com/api/v3/athlete/activity";
+
     void requestAccessToken(std::string access_token);
-    void setHeaders(curl_slist* headers);
-    void getAthlete(std::string access_token, );
+    void setHeaders();
+    void makeRequest();
 
 };
 
