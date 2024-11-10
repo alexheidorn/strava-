4f18d7aa6cf92973539b94b63e1cc113bb376925
Alex Heidorn <heido023@d.umn.edu>
2024-11-10 16:02:45 -0600

    write test - issue opneing file

--- tests/nlohmann_test.cpp
+++ tests/nlohmann_test.cpp
@@ -2,10 +2,26 @@
 // Created by alexh on 11/10/2024.
 //
 
+#include <fstream>
+#include <iostream>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
 int main() {
+    // Open example JSON file
+    std::ifstream file("JSONs/example.json");
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file" << std::endl;
+        return 1;
+    }
+    
+    json data = json::parse(file);
 
+    std::cout << data << std::endl;
+
+    file.close();
+    
+    return 0;
 }
