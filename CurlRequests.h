2fc8fe2876c34ea4aeaa04e06b85d74e34d95a67
Alex Heidorn <heido023@d.umn.edu>
2024-11-10 16:01:39 -0600

    include authoriztion tokens header

--- CurlRequests.h
+++ CurlRequests.h
@@ -9,6 +9,8 @@
 #include <curl/curl.h>
 #include <string>
 
+#include "AuthorizationTokens.h"
+
 // Callback function to handle the API response
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
