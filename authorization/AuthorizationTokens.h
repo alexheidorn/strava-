22d32581275b79298a0a0cec9f4ea7afed646254
Alex Heidorn <heido023@d.umn.edu>
2024-11-10 15:32:20 -0600

    made accessToken private. creator settters for AuthCode & accessToken

--- authorization/AuthorizationTokens.h
+++ authorization/AuthorizationTokens.h
@@ -13,18 +13,27 @@ class AuthorizationTokens {
         std::string clientSecret = "f8faf24737d498daba841d1ed163f82c700dc84a";
         std::string authorizationCode = "7f312f64b54108f2fd2aec898bc275e2221c6a09";
 
+        std::string access_token = "1693f696624a145e0657104cb332df3fe28b3675";
+
         std::string redirectURI = "http://localhost/exchange_token";
         std::string scope = "read";
         std::string responseType = "code";
 
     public:
-        std::string access_token = "1693f696624a145e0657104cb332df3fe28b3675";
+        
         std::string refresh_token;
         std::string access_token_expires_at;
         std::string refresh_token_expires_at;
 
         std::string getClientID();
         std::string getClientSecret();
+        std::string getAuthorizationCode();
+        std::string getAccessToken();
+
+        void setAuthorizationCode(std::string authorizationCode);
+        
+        void setAccessToken(std::string access_token);
+
 };
 
 
