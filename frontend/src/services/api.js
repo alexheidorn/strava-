9eae68fb8b5853ae60af71d47577b57b5e7899f3
alexheidorn <alexheidorn12@gmail.com>
2025-05-14 00:00:11 -0500

    use proxy for backend access

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000', // or wherever your backend is running
+  baseURL: '/api', // nginx will proxy /api to backend
 });
