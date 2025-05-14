6c784a0d2c126446e13539cf0817964f32050e4c
alexheidorn <alexheidorn12@gmail.com>
2025-05-13 23:55:41 -0500

    show chart

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ActivityChart from './components/ActivityChart';
+
+function App() {
+  return (
+    <div className="App">
+      <ActivityChart />
+    </div>
+  );
+}
+
+export default App;
