cb0a15cdd2eb45b7c28a415f0e123aecec119d05
alexheidorn <alexheidorn12@gmail.com>
2025-05-13 22:22:18 -0500

    add mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 const stravaRoutes = require("./routes/stravaRoutes");
 
